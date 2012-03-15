e86ced5084121bef085742df2229b7e473b60825
Lucas SOLTIC <ceylow@gmail.com>
2012-03-15 16:28:12 +0100

    Disabled SDK prompt for OS != Mac OS X. Re-added support for different versions of Visual Studio. Disabled FFmpeg documentation building. Fixed temporary PATH change to run lib.exe (was preventing cmake from correctly running). Fixed sfeMovie solution not being build when choosing Visual Studio as target environment.

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ linking=""
 macosx_arch="x86_64" # ppc or i386 or x86_64
 macosx_sdk=""
 has_vda="0"
+vcpp=0
 use_cache=1
 jobsCount=4 # how many compilations at a time
 codec_list="aac aac_latm aasc ac3 adpcm_4xm adpcm_adx adpcm_ct adpcm_ea adpcm_ea_maxis_xa adpcm_ea_r1 adpcm_ea_r2 adpcm_ea_r3 adpcm_ea_xas adpcm_g722 adpcm_g726 adpcm_ima_amv adpcm_ima_dk3 adpcm_ima_dk4 adpcm_ima_ea_eacs adpcm_ima_ea_sead adpcm_ima_iss adpcm_ima_qt adpcm_ima_smjpeg adpcm_ima_wav adpcm_ima_ws adpcm_ms adpcm_sbpro_2 adpcm_sbpro_3 adpcm_sbpro_4 adpcm_swf adpcm_thp adpcm_xa adpcm_yamaha aea aiff alac als amr amrnb amrwb amv anm ansi apc ape applehttp asf ass asv1 asv2 atrac1 atrac3 au aura aura2 avi avisynth avs bethsoftvid bfi bink binkaudio_dct binkaudio_rdft bmp c93 caf cavs cavsvideo cdg cdgraphics cinepak cljr cook cscd cyuv daud dca dfa dirac dnxhd dpx dsicin dsicinaudio dsicinvideo dts dv dvbsub dvdsub dvvideo dxa ea ea_cdata eac3 eacmv eamad eatgq eatgv eatqi eightbps eightsvx_exp eightsvx_fib eightsvx_raw escape124 ffm ffmetadata ffv1 ffvhuff filmstrip flac flashsv flic flv fourxm fraps frwu g722 gif gsm gsm_ms gxf h261 h263 h263i h264 h264_crystalhd h264_vdpau huffyuv idcin iff iff_byterun1 iff_ilbm image2 image2pipe imc indeo2 indeo3 indeo5 ingenient interplay_dpcm interplay_video ipmovie iss iv8 ivf jpeg2000 jpegls jv kgv1 kmvc lagarith libcelt libdirac libgsm libgsm_ms libnut libopencore_amrnb libopencore_amrwb libopenjpeg libschroedinger libspeex libvpx lmlm4 loco lxf m4v mace3 mace6 matroska mdec microdvd mimic mjpeg mjpegb mlp mm mmf mmvideo motionpixels mov mp1 mp1float mp2 mp2float mp3 mp3adu mp3adufloat mp3float mp3on4 mp3on4float mpc mpc7 mpc8 mpeg1_vdpau mpeg1video mpeg2_crystalhd mpeg2video mpeg4 mpeg4_crystalhd mpeg4_vdpau mpeg_vdpau mpeg_xvmc mpegps mpegts mpegtsraw mpegvideo msmpeg4_crystalhd msmpeg4v1 msmpeg4v2 msmpeg4v3 msnwc_tcp msrle msvideo1 mszh mtv mvi mxf mxg mxpeg nc nellymoser nsv nut nuv ogg oma pam pbm pcm_alaw pcm_bluray pcm_dvd pcm_f32be pcm_f32le pcm_f64be pcm_f64le pcm_lxf pcm_mulaw pcm_s16be pcm_s16le pcm_s16le_planar pcm_s24be pcm_s24daud pcm_s24le pcm_s32be pcm_s32le pcm_s8 pcm_u16be pcm_u16le pcm_u24be pcm_u24le pcm_u32be pcm_u32le pcm_u8 pcm_zork pcx pgm pgmyuv pgssub pictor pmp png ppm ptx pva qcelp qcp qdm2 qdraw qpeg qtrle r10k r210 r3d ra_144 ra_288 rawvideo rl2 rm roq roq_dpcm rpl rpza rso rtp rtsp rv10 rv20 rv30 rv40 s302m sap sdp segafilm sgi shorten siff sipr smackaud smacker smc snow sol sol_dpcm sonic sox sp5x spdif srt str sunrast svq1 svq3 swf targa theora thp tiertexseq tiertexseqvideo tiff tmv truehd truemotion1 truemotion2 truespeech tscc tta tty twinvq txd ulti v210 v210x vb vc1 vc1_crystalhd vc1_vdpau vc1t vcr1 vmd vmdaudio vmdvideo vmnc voc vorbis vp3 vp5 vp6 vp6a vp6f vp8 vqa vqf w64 wav wavpack wc3 wmapro wmav1 wmav2 wmavoice wmv1 wmv2 wmv3 wmv3_crystalhd wmv3_vdpau wnv1 ws_snd1 wsaud wsvqa wtv wv xa xan_dpcm xan_wc3 xan_wc4 xl xsub xwma yop yuv4mpegpipe zlib zmbv "
@@ -50,49 +51,49 @@ function setup()
 		    macosx_107_sdk="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk";
 		    has_107=1
 		fi
-    fi
     
-    if [ "$has_105" == "" ] && [ "$has_106" == "" ] && [ "$has_107" == "" ]
-      then
-        echo "Could not find any SDK. Sources will be built without specifying any SDK."
-    else
-    	echo "Choose the Mac OS X SDK you want to link against (default is 0):"
-    	echo "0. None (use current OS headers)"
-    	
-    	if [ "$has_105" == "1" ]
-    	  then
-    	    echo "5. Mac OS X 10.5 SDK"
-    	fi
-    	
-    	if [ "$has_106" == "1" ]
-    	  then
-    	    echo "6. Mac OS X 10.6 SDK"
-    	fi
-    	
-    	if [ "$has_107" == "1" ]
-    	  then
-    	    echo "7. Mac OS X 10.7 SDK"
-    	fi
-    	
-    	read choice
-    	
-    	if [ "$choice" == "5" ] && [ "$has_105" == "1" ]
-    	  then
-    	    macosx_sdk=$macosx_105_sdk
-    	elif [ "$choice" == "6" ] && [ "$has_106" == "1" ]
-    	  then
-    	    macosx_sdk=$macosx_106_sdk
-    	elif [ "$choice" == "7" ] && [ "$has_107" == "1" ]
-    	  then
-    	    macosx_sdk=$macosx_107_sdk
-		    has_vda=1
-    	fi
-    	
-    	if [ "$macosx_sdk" != "" ]
-    	  then
-    	    echo "Selected SDK: $macosx_sdk"
-    	fi
-    fi
+		if [ "$has_105" == "" ] && [ "$has_106" == "" ] && [ "$has_107" == "" ]
+		  then
+			echo "Could not find any SDK. Sources will be built without specifying any SDK."
+		else
+			echo "Choose the Mac OS X SDK you want to link against (default is 0):"
+			echo "0. None (use current OS headers)"
+			
+			if [ "$has_105" == "1" ]
+			  then
+				echo "5. Mac OS X 10.5 SDK"
+			fi
+			
+			if [ "$has_106" == "1" ]
+			  then
+				echo "6. Mac OS X 10.6 SDK"
+			fi
+			
+			if [ "$has_107" == "1" ]
+			  then
+				echo "7. Mac OS X 10.7 SDK"
+			fi
+			
+			read choice
+			
+			if [ "$choice" == "5" ] && [ "$has_105" == "1" ]
+			  then
+				macosx_sdk=$macosx_105_sdk
+			elif [ "$choice" == "6" ] && [ "$has_106" == "1" ]
+			  then
+				macosx_sdk=$macosx_106_sdk
+			elif [ "$choice" == "7" ] && [ "$has_107" == "1" ]
+			  then
+				macosx_sdk=$macosx_107_sdk
+				has_vda=1
+			fi
+			
+			if [ "$macosx_sdk" != "" ]
+			  then
+				echo "Selected SDK: $macosx_sdk"
+			fi
+		fi
+	fi
 }
 
 function build_ffmpeg()
@@ -102,8 +103,10 @@ function build_ffmpeg()
 	  then
 		echo "Choose your target environment (default is 1):"
 		echo "1. MinGW"
-		echo "2. Visual Studio"
-		echo "3. Other"
+		echo "2. Visual Studio 2005"
+		echo "3. Visual Studio 2008"
+		echo "4. Visual Studio 2010"
+		echo "5. Other"
 		echo ""
 		
 		read tenv
@@ -113,7 +116,16 @@ function build_ffmpeg()
 			cmake_env="MSYS Makefiles"
 		elif [ "$tenv" == "2" ]
 		  then
-			cmake_env="Visual Studio"
+			cmake_env="Visual Studio 8 2005"
+			vcpp=1
+		elif [ "$tenv" == "3" ]
+		  then
+			cmake_env="Visual Studio 9 2008"
+			vcpp=1
+		elif [ "$tenv" == "4" ]
+		  then
+			cmake_env="Visual Studio 10"
+			vcpp=1
 		else
 			echo "This script does not support any other environment."
 			exit 1
@@ -123,7 +135,7 @@ function build_ffmpeg()
 		linking="static"
 	fi
 	
-	if [ "$cmake_env" == "Visual Studio" ]
+	if [ "$vcpp" == "1" ]
 	  then
 		args="$args --enable-shared --disable-static"
 	fi
@@ -131,7 +143,7 @@ function build_ffmpeg()
 	has_ffmpeg_binaries=0
 	if [ "$use_cache" == "1" ]
 	  then
-		if [ "$cmake_env" == "Visual Studio" ]
+		if [ "$vcpp" == "1" ]
 		  then
 			if test -d deps/ffmpeg-build/ &&
 				test -f deps/ffmpeg-build/avcodec.dll &&
@@ -281,11 +293,11 @@ What is your choice? [1-4] (default is 1)"
 			    os_flags="--enable-memalign-hack --enable-w32threads"
 			fi
 			
-			args="$args --disable-ffmpeg --disable-ffplay --disable-ffprobe --disable-ffserver --disable-encoders --disable-decoders --disable-yasm $configure_flags $os_flags"
+			args="$args --disable-ffmpeg --disable-ffplay --disable-ffprobe --disable-ffserver --disable-doc --disable-encoders --disable-decoders --disable-yasm $configure_flags $os_flags"
 	        
 			
 			#setup VC++ env variables to find lib.exe
-			if [ "$os" == "windows" ]
+			if [ "$vcpp" == "1" ]
 			  then
 				old_path=`echo $PATH`
 				export PATH="$PATH:/C/Program Files/Microsoft Visual Studio 9.0/Common7/IDE:/C/Program Files/Microsoft Visual Studio 9.0/VC/bin"
@@ -304,16 +316,16 @@ What is your choice? [1-4] (default is 1)"
 	        make --jobs=$jobsCount
 	        check_err
 	        
-	        if [ "$os" == "windows" ]
+	        if [ "$vcpp" == "1" ]
 	          then
-				export PATH="\"$old_path\""
+				export PATH="$old_path"
 			fi
 	        
 		    mkdir -p ../ffmpeg-build
 			rm ../ffmpeg-build/*
 		    
 			echo "Copying libraries into ffmpeg-build"
-			if [ "$cmake_env" == "Visual Studio" ]
+			if [ "$vcpp" == "1" ]
 			  then
 				cp -v `find . -name "*.lib"` ../ffmpeg-build
 				check_err
@@ -346,8 +358,19 @@ function build_sfemovie()
 	  then
 	    rm CMakeCache.txt
 	fi
+	echo "Running CMake..."
+		
+	if [ "$macosx_sdk" != "" ]
+	  then
+		echo "cmake -G \"$cmake_env\" -DCMAKE_OSX_SYSROOT:STRING=\"$macosx_sdk\" -DHAS_VDA:STRING=\"$has_vda\" CMakeLists.txt"
+		cmake -G "$cmake_env" -DCMAKE_OSX_SYSROOT:STRING="$macosx_sdk" -DHAS_VDA="$has_vda" CMakeLists.txt
+	else
+		echo "cmake -G \"$cmake_env\" CMakeLists.txt"
+		cmake -G "$cmake_env" CMakeLists.txt
+	fi
+	check_err
 	
-	if [ "$cmake_env" == "Visual Studio" ]
+	if [ "$vcpp" == "1" ]
 	  then
 	    echo ""
 	    echo "The files required to build sfeMovie for Visual Studio have been created."
@@ -356,18 +379,6 @@ function build_sfemovie()
 		echo "This script is over."
 		exit 0
 	else
-		echo "Running CMake..."
-		
-		if [ "$macosx_sdk" != "" ]
-		  then
-			echo "cmake -G \"$cmake_env\" -DCMAKE_OSX_SYSROOT:STRING=\"$macosx_sdk\" -DHAS_VDA:STRING=\"$has_vda\" CMakeLists.txt"
-			cmake -G "$cmake_env" -DCMAKE_OSX_SYSROOT:STRING="$macosx_sdk" -DHAS_VDA="$has_vda" CMakeLists.txt
-		else
-			echo "cmake -G \"$cmake_env\" CMakeLists.txt"
-			cmake -G "$cmake_env" CMakeLists.txt
-		fi
-		check_err
-	
 		echo "Running make..."
 		make --jobs=$jobsCount
 		check_err
