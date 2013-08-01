d20675ca16f3a64e45bf13b32d22725bc1facf71
Lucas SOLTIC <lucas.soltic@orange.fr>
2013-08-01 23:05:32 +0200

    #19 / #31 FFmpeg build now correctly launched when building from Visual Studio 2012 (but fails)

--- RunShellCommand.cmake
+++ RunShellCommand.cmake
@@ -4,19 +4,15 @@ function(RunShell target phase shell_command)
 	list(REMOVE_AT cmd 0 1)
 	
 	if (MSVC)
-		add_custom_command(TARGET ${target}
-                     ${phase}
-                     COMMAND BatchBridgeToShell ARGS ${MINGW_DIR} ${cmd}
-					 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})	
+		add_custom_target(BuildFFMPEG ALL DEPENDS "ffmpeg-libs") 
+		add_custom_command(OUTPUT "ffmpeg-libs"
+					COMMAND BatchBridgeToShell ARGS ${MINGW_DIR} ${cmd}
+					WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 	else()
 		message(STATUS "add_custom_command(TARGET ${target} ${phase} COMMAND bash ARGS -c \"${cmd}\" WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}")
 		add_custom_target(BuildFFMPEG ALL DEPENDS "ffmpeg-libs") 
 		add_custom_command(OUTPUT "ffmpeg-libs"
 					COMMAND bash ARGS -c \"${cmd}\"
 					WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
-		# add_custom_command(TARGET ${target}
-  #                    ${phase}
-  #                    COMMAND bash ARGS -c \"${cmd}\"
-		# 			 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})	
 	endif()
 endfunction(RunShell)
