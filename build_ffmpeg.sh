fe0fdf748dde36e952cd39cae948241b5a51ef9d
Lucas Soltic <lucas.soltic@orange.fr>
2013-08-28 13:57:14 +0200

    #24 / #29 Storing FFmpeg as a tar.bz2 archive to avoid line ending issues on Windows, and extracting files to /tmp to avoid issues when the sfeMovie sources directory's path contains spaces

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -6,6 +6,7 @@ linking=""
 macosx_arch="x86_64" # ppc or i386 or x86_64
 source_dir=""
 build_dir=""
+temporary_dir="/tmp"
 macosx_sdk=""
 has_vda=0
 vcpp=0
@@ -23,6 +24,8 @@ function check_err()
 
 function build_ffmpeg()
 {
+	cd "${build_dir}"
+
 	# Using dynamically linked libraries for MSVC and Linux,
 	# and static libs for Codeblocks (Windows) and Mac OS X
 	if [ "$vcpp" == "1" ] || [ "$os" == "linux" ]
@@ -31,96 +34,101 @@ function build_ffmpeg()
 	fi
 	
     # build ffmpeg
-    if test -d "${source_dir}/deps/ffmpeg"
-      then
-        configure_flags="";
-        
-        for codec in $full_decoders_list
-          do
-            configure_flags="$configure_flags --enable-decoder=$codec"
-        done
-        
-        if [ "$os" == "macosx" ]
-          then
-          	if [ "$macosx_sdk" != "" ]
-          	  then
-          	    os_flags="$os_flags --sysroot=$macosx_sdk"
-          	fi
-          	
-          	if [ "$has_vda" == "0" ]
-          	  then
-          	    os_flags="$os_flags --disable-vda"
-          	fi
-          		
-        	#os_flags="$os_flags --cc=\"gcc -arch $macosx_arch\" --arch=$macosx_arch --target-os=darwin --enable-cross-compile --host-cflags=\"-arch $macosx_arch\" --host-ldflags=\"-arch $macosx_arch\""
-        fi
-		
-		if [ "$os" == "windows" ]
-		  then
-			os_flags="--enable-memalign-hack --enable-w32threads"
-		fi
-	
-		args="$args --disable-ffmpeg --disable-ffplay --disable-ffprobe --disable-ffserver --disable-doc --disable-encoders --disable-decoders --disable-muxers --disable-yasm $configure_flags $os_flags"
-	    
-	
-		#setup VC++ env variables to find lib.exe
-		if [ "$vcpp" == "1" ]
-		  then
-			old_path=`echo $PATH`
-			export PATH="$PATH:/C/Program Files/Microsoft Visual Studio 9.0/Common7/IDE:/C/Program Files/Microsoft Visual Studio 9.0/VC/bin"
-			export PATH="$PATH:/C/Program Files/Microsoft Visual Studio 10.0/Common7/IDE:/C/Program Files/Microsoft Visual Studio 10.0/VC/bin"
-			export PATH="$PATH:/C/Program Files/Microsoft Visual Studio 11.0/Common7/IDE:/C/Program Files/Microsoft Visual Studio 11.0/VC/bin"
-			export PATH="$PATH:/C/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/IDE:/C/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin"
-			export PATH="$PATH:/C/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE:/C/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin"
-			export PATH="$PATH:/C/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/IDE:/C/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin"
-		fi
-		
-        chmod u+x "${source_dir}/deps/ffmpeg/configure" "${source_dir}/deps/ffmpeg/version.sh" "${source_dir}/deps/ffmpeg/doc/texi2pod.pl"
-        mkdir -p "${build_dir}/FFmpeg-objects"
-        cd "${build_dir}/FFmpeg-objects"
-
-        echo "${source_dir}/deps/ffmpeg/configure $args"
-        { echo "$args" | xargs "${source_dir}/deps/ffmpeg/configure"; }
-        check_err
-        make clean
-        check_err
-        make --jobs=$jobsCount
-        check_err
-        
-        if [ "$vcpp" == "1" ]
-          then
-			export PATH="$old_path"
-		fi
-        
-        ffmpeg_sources_dir="${source_dir}/deps/ffmpeg"
-        ffmpeg_objects_dir="${build_dir}/FFmpeg-objects"
-        ffmpeg_binaries_dir="${build_dir}/FFmpeg-binaries"
-
-		mkdir -p "${ffmpeg_binaries_dir}"
-		rm -f "${ffmpeg_binaries_dir}/*"
-	    
-		echo "Copying libraries into ${ffmpeg_binaries_dir}"
-		if [ "$vcpp" == "1" ]
-		  then
-			find "${ffmpeg_objects_dir}" -name "*.lib" -exec cp -v '{}' "${ffmpeg_binaries_dir}" ';' 
-			check_err
-			find "${ffmpeg_objects_dir}" -name "*.dll" -exec cp -v '{}' "${ffmpeg_binaries_dir}" ';'
-			check_err
-		else
-			if [ "$os" == "linux" ]
-			  then
-			    find "${ffmpeg_objects_dir}" -name "*.so*" -exec cp -vfl '{}' "${ffmpeg_binaries_dir}" ';' 
-			else
-				find "${ffmpeg_objects_dir}" -name "*.a" -exec cp -v '{}' "${ffmpeg_binaries_dir}" ';' 
-			fi
-			check_err
-		fi
-		
-        cd ${source_dir}
+    if ! test -f "${temporary_dir}/ffmpeg/configure" ; then
+    	if test -f "${source_dir}/deps/ffmpeg.tar.bz2" ; then
+    		echo "Extracting FFmpeg archive..."
+    		tar -C "${temporary_dir}" -xjf "${source_dir}/deps/ffmpeg.tar.bz2"
+    		check_err
+    	else
+    		echo "Cannot find FFmpeg sources in temporary directory or FFmpeg archive at ${source_dir}/deps/ffmpeg.tar.bz2"
+    		exit 1
+    	fi
     else
-    	echo "Missing ffmpeg sources directory at ${source_dir}/deps/ffmpeg. Aborting."
-    	exit 1
+    	echo "FFmpeg sources found, skipping archive extraction"
     fi
+
+    configure_flags="";
+    
+    for codec in $full_decoders_list
+      do
+        configure_flags="$configure_flags --enable-decoder=$codec"
+    done
+    
+    if [ "$os" == "macosx" ]
+      then
+      	if [ "$macosx_sdk" != "" ]
+      	  then
+      	    os_flags="$os_flags --sysroot=$macosx_sdk"
+      	fi
+      	
+      	if [ "$has_vda" == "0" ]
+      	  then
+      	    os_flags="$os_flags --disable-vda"
+      	fi
+      		
+    	#os_flags="$os_flags --cc=\"gcc -arch $macosx_arch\" --arch=$macosx_arch --target-os=darwin --enable-cross-compile --host-cflags=\"-arch $macosx_arch\" --host-ldflags=\"-arch $macosx_arch\""
+    fi
+	
+	if [ "$os" == "windows" ]
+	  then
+		os_flags="--enable-memalign-hack --enable-w32threads"
+	fi
+
+	args="$args --disable-ffmpeg --disable-ffplay --disable-ffprobe --disable-ffserver --disable-doc --disable-encoders --disable-decoders --disable-muxers --disable-yasm $configure_flags $os_flags"
+    
+
+	#setup VC++ env variables to find lib.exe
+	if [ "$vcpp" == "1" ]
+	  then
+		old_path=`echo $PATH`
+		export PATH="$PATH:/C/Program Files/Microsoft Visual Studio 9.0/Common7/IDE:/C/Program Files/Microsoft Visual Studio 9.0/VC/bin"
+		export PATH="$PATH:/C/Program Files/Microsoft Visual Studio 10.0/Common7/IDE:/C/Program Files/Microsoft Visual Studio 10.0/VC/bin"
+		export PATH="$PATH:/C/Program Files/Microsoft Visual Studio 11.0/Common7/IDE:/C/Program Files/Microsoft Visual Studio 11.0/VC/bin"
+		export PATH="$PATH:/C/Program Files (x86)/Microsoft Visual Studio 9.0/Common7/IDE:/C/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin"
+		export PATH="$PATH:/C/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE:/C/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin"
+		export PATH="$PATH:/C/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/IDE:/C/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin"
+	fi
+	
+    chmod u+x "${temporary_dir}/ffmpeg/configure" "${temporary_dir}/ffmpeg/version.sh" "${temporary_dir}/ffmpeg/doc/texi2pod.pl"
+    mkdir -p "${build_dir}/FFmpeg-objects"
+    cd "${build_dir}/FFmpeg-objects"
+
+    echo "${temporary_dir}/ffmpeg/configure $args"
+    { echo "$args" | xargs "${temporary_dir}/ffmpeg/configure"; }
+    check_err
+    make clean
+    check_err
+    make --jobs=$jobsCount
+    check_err
+    
+    if [ "$vcpp" == "1" ]
+      then
+		export PATH="$old_path"
+	fi
+    
+    ffmpeg_sources_dir="${temporary_dir}/ffmpeg"
+    ffmpeg_objects_dir="${build_dir}/FFmpeg-objects"
+    ffmpeg_binaries_dir="${build_dir}/FFmpeg-binaries"
+
+	mkdir -p "${ffmpeg_binaries_dir}"
+	rm -f "${ffmpeg_binaries_dir}/*"
+    
+	echo "Copying libraries into ${ffmpeg_binaries_dir}"
+	if [ "$vcpp" == "1" ]
+	  then
+		find "${ffmpeg_objects_dir}" -name "*.lib" -exec cp -v '{}' "${ffmpeg_binaries_dir}" ';' 
+		check_err
+		find "${ffmpeg_objects_dir}" -name "*.dll" -exec cp -v '{}' "${ffmpeg_binaries_dir}" ';'
+		check_err
+	else
+		if [ "$os" == "linux" ]
+		  then
+		    find "${ffmpeg_objects_dir}" -name "*.so*" -exec cp -vfl '{}' "${ffmpeg_binaries_dir}" ';' 
+		else
+			find "${ffmpeg_objects_dir}" -name "*.a" -exec cp -v '{}' "${ffmpeg_binaries_dir}" ';' 
+		fi
+		check_err
+	fi
 	
 	echo "Built ffmpeg"
 }
