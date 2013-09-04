da411a75af40f934d66723ddb222b4618237364e
Lucas Soltic <lucas.soltic@orange.fr>
2013-09-04 16:57:08 +0200

    #21 Update target name

--- RunShellCommand.cmake
+++ RunShellCommand.cmake
@@ -4,13 +4,13 @@ function(RunShell target phase shell_command)
 	list(REMOVE_AT cmd 0 1)
 	
 	if (MSVC)
-		add_custom_target(BuildFFMPEG ALL DEPENDS ${FFMPEG_LIBRARIES}) 
+		add_custom_target(FFmpeg ALL DEPENDS ${FFMPEG_LIBRARIES}) 
 		add_custom_command(OUTPUT ${FFMPEG_LIBRARIES}
 					COMMAND BatchBridgeToShell ARGS ${MINGW_DIR} ${cmd}
 					DEPENDS "${CMAKE_BINARY_DIR}/CMakeCache.txt"
 					WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
 	else()
-		add_custom_target(BuildFFMPEG ALL DEPENDS ${FFMPEG_LIBRARIES}) 
+		add_custom_target(FFmpeg ALL DEPENDS ${FFMPEG_LIBRARIES}) 
 		add_custom_command(OUTPUT ${FFMPEG_LIBRARIES}
 					COMMAND bash ARGS -c \"${cmd}\"
 					DEPENDS "${CMAKE_BINARY_DIR}/CMakeCache.txt"
