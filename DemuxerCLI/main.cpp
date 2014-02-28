fb4024924a57630fcdb70d83c6b87ef42ecb7c1c
Lucas SOLTIC <lucas.soltic@orange.fr>
2014-02-28 17:23:46 +0100

    #34 Added Demuxer CLI for testing (shows available decoders, media streams and plays audio)

---
+++ DemuxerCLI/main.cpp
@@ -0,0 +1,39 @@
+
+#include <SFML/Config.hpp>
+#include "Demuxer.hpp"
+#include "Utilities.hpp"
+#include <iostream>
+
+void pause()
+{
+#ifdef SFML_SYSTEM_WINDOWS
+	system("PAUSE");
+#endif
+}
+
+int main(int argc, const char *argv[])
+{
+	// Some settings
+	
+	if (argc < 2)
+	{
+		std::cout << "Usage: " << std::string(argv[0]) << " media_path" << std::endl;
+		pause();
+		return 1;
+	}
+	
+	std::string mediaFile = std::string(argv[1]);
+	std::cout << "Going to open movie file \"" << mediaFile << "\"" << std::endl;
+	
+	sfe::dumpAvailableDecoders();
+	
+	sfe::Timer timer;
+	sfe::Demuxer demuxer(mediaFile, timer);
+	timer.play();
+	
+	while (!demuxer.didReachEndOfFile())
+		sf::sleep(sf::milliseconds(100));
+	
+	return 0;
+}
+
