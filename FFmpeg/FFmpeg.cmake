683ec214e3cca6a096a49b295b3b7cfb9c6692f3
Ceylo <lucas.soltic@orange.fr>
2014-08-28 23:08:15 +0200

    #51 Fix FFmpeg lib suffix on non Windows target

--- FFmpeg/FFmpeg.cmake
+++ FFmpeg/FFmpeg.cmake
@@ -6,6 +6,11 @@ macro(import_library_path output basePath libName)
 	else()
 		set(SUFFIX ${CMAKE_IMPORT_LIBRARY_SUFFIX})
 	endif()
+
+	if (NOT "${SUFFIX}")
+		set(SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+	endif()
+
 	set(${output} "${basePath}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}${libName}${SUFFIX}")
 endmacro(import_library_path)
 
