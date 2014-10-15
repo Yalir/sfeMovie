7f57a26cb043cad6d37e08162221d27fa0bce474
Stephan Vedder <stephan.vedder@gmail.com>
2014-10-15 22:34:56 +0200

    Did replace tabs with whitespaces and put every {} in a seperate line

--- src/SubtitleStream.cpp
+++ src/SubtitleStream.cpp
@@ -36,171 +36,175 @@ extern "C" {
 
 
 namespace sfe {
-	const int RGBASize = 4;
+    const int RGBASize = 4;
 
-	SubtitleStream::SubtitleStream(AVFormatContext* formatCtx, AVStream* stream, DataSource& dataSource, Timer& timer,Delegate& delegate) :
-		Stream(formatCtx, stream, dataSource, timer), m_delegate(delegate)
-	{
-	}
+    SubtitleStream::SubtitleStream(AVFormatContext* formatCtx, AVStream* stream, DataSource& dataSource, Timer& timer, Delegate& delegate) :
+        Stream(formatCtx, stream, dataSource, timer), m_delegate(delegate)
+    {
+    }
 
-	/** Default destructor
-	*/
-	SubtitleStream::~SubtitleStream()
-	{
+    /** Default destructor
+    */
+    SubtitleStream::~SubtitleStream()
+    {
 
-	}
+    }
 
-	MediaType SubtitleStream::getStreamKind() const
-	{
-		return Subtitle;
-	}
+    MediaType SubtitleStream::getStreamKind() const
+    {
+        return Subtitle;
+    }
 
-	void SubtitleStream::update()
-	{
-		//only get new subtitles if we are running low
-		if (m_status == Playing && hasPackets())
-		{
-				if (!onGetData()) {
-				setStatus(Stopped);
-			}
-		}
+    void SubtitleStream::update()
+    {
+        //only get new subtitles if we are running low
+        if (m_status == Playing && hasPackets())
+        {
+            if (!onGetData()) {
+                setStatus(Stopped);
+            }
+        }
 
-		if(m_pendingSubtitles.size() > 0)
-		{
-			//activate subtitle
-			if (m_pendingSubtitles.front()->start < m_timer.getOffset())
-			{
-				SubtitleData* iter = m_pendingSubtitles.front();
-                
-				m_delegate.didUpdateSubtitle(*this, iter->sprites,iter->positions);
-				m_visibleSubtitles.push_back(iter);
-				m_pendingSubtitles.pop_front();
-			}
-		}
-					
+        if (m_pendingSubtitles.size() > 0)
+        {
+            //activate subtitle
+            if (m_pendingSubtitles.front()->start < m_timer.getOffset())
+            {
+                SubtitleData* iter = m_pendingSubtitles.front();
 
-		if (m_visibleSubtitles.size()>0)
-		{
-			//remove subtitle
-			if (m_visibleSubtitles.front()->end < m_timer.getOffset())
-			{
+                m_delegate.didUpdateSubtitle(*this, iter->sprites, iter->positions);
+                m_visibleSubtitles.push_back(iter);
+                m_pendingSubtitles.pop_front();
+            }
+        }
+
+
+        if (m_visibleSubtitles.size()>0)
+        {
+            //remove subtitle
+            if (m_visibleSubtitles.front()->end < m_timer.getOffset())
+            {
                 SubtitleData* subtitle = m_visibleSubtitles.front();
-				m_visibleSubtitles.pop_front();
-			
-				if (m_visibleSubtitles.size() == 0)
-				{
-					m_delegate.didUpdateSubtitle(*this, std::list<sf::Sprite>(), std::list<sf::Vector2i>());
+                m_visibleSubtitles.pop_front();
+
+                if (m_visibleSubtitles.size() == 0)
+                {
+                    m_delegate.didUpdateSubtitle(*this, std::list<sf::Sprite>(), std::list<sf::Vector2i>());
                     delete subtitle;
-				}
-			}
-		}			
-	}
+                }
+            }
+        }
+    }
 
-	bool SubtitleStream::onGetData()
-	{
-		AVPacket* packet = popEncodedData();
-		AVSubtitle sub;
-		int32_t gotSub = 0;
-		uint32_t goOn = 0;
-		int64_t pts = 0;
+    bool SubtitleStream::onGetData()
+    {
+        AVPacket* packet = popEncodedData();
+        AVSubtitle sub;
+        int32_t gotSub = 0;
+        uint32_t goOn = 0;
+        int64_t pts = 0;
 
-		if (packet) {
-			goOn = 1;
+        if (packet)
+        {
+            goOn = 1;
 
-			while (!gotSub && packet && goOn) {
-				bool needsMoreDecoding = false;
+            while (!gotSub && packet && goOn)
+            {
+                bool needsMoreDecoding = false;
 
-				CHECK(packet != NULL, "inconsistency error");
-				goOn = avcodec_decode_subtitle2(m_codecCtx, &sub, &gotSub, packet);
+                CHECK(packet != NULL, "inconsistency error");
+                goOn = avcodec_decode_subtitle2(m_codecCtx, &sub, &gotSub, packet);
 
-				pts = 0;
-				if (packet->pts != AV_NOPTS_VALUE)
-					pts =  packet->pts;
+                pts = 0;
+                if (packet->pts != AV_NOPTS_VALUE)
+                    pts = packet->pts;
 
-				if (gotSub && pts) {
-					SubtitleData* sfeSub = new SubtitleData(&sub);
-					m_pendingSubtitles.push_back(sfeSub);
-				}
+                if (gotSub && pts)
+                {
+                    SubtitleData* sfeSub = new SubtitleData(&sub);
+                    m_pendingSubtitles.push_back(sfeSub);
+                }
 
-				if (needsMoreDecoding) {
-					prependEncodedData(packet);
-				}
-				else {
-					av_free_packet(packet);
-					av_free(packet);
-				}
+                if (needsMoreDecoding)
+                {
+                    prependEncodedData(packet);
+                }
+                else
+                {
+                    av_free_packet(packet);
+                    av_free(packet);
+                }
 
-				if (!gotSub && goOn) {
-					sfeLogDebug("no subtitle in this packet, reading further");
-					packet = popEncodedData();
-				}
-			}
-		}
+                if (!gotSub && goOn) {
+                    sfeLogDebug("no subtitle in this packet, reading further");
+                    packet = popEncodedData();
+                }
+            }
+        }
+        return static_cast<bool>(goOn);
+    }
 
-		return static_cast<bool>(goOn);
-	}
 
-
-	SubtitleStream::SubtitleData::SubtitleData(AVSubtitle* sub)
-	{
+    SubtitleStream::SubtitleData::SubtitleData(AVSubtitle* sub)
+    {
         assert(sub != NULL);
-        
-		start = sf::milliseconds(sub->start_display_time) + sf::microseconds(sub->pts);
-		end = sf::milliseconds(sub->end_display_time) + sf::microseconds(sub->pts);
-		
-		for (int i = 0; i < sub->num_rects; ++i)
-		{
-			
-			sprites.push_back(sf::Sprite());
+
+        start = sf::milliseconds(sub->start_display_time) + sf::microseconds(sub->pts);
+        end = sf::milliseconds(sub->end_display_time) + sf::microseconds(sub->pts);
+
+        for (int i = 0; i < sub->num_rects; ++i)
+        {
+
+            sprites.push_back(sf::Sprite());
             textures.push_back(sf::Texture());
-            
-			sf::Sprite& sprite = sprites.back();
-			sf::Texture& texture = textures.back();
-			AVSubtitleRect* subItem = sub->rects[i];
 
-			AVSubtitleType type = subItem->type;
-            
-			if (type == SUBTITLE_BITMAP)
-			{
-				positions.push_back(sf::Vector2i(subItem->x, subItem->y));
-				uint32_t* palette = new uint32_t[subItem->nb_colors];
-				for (int j = 0; j < subItem->nb_colors; j++)
-					palette[j] = *(uint32_t*)&subItem->pict.data[1][j * RGBASize];
+            sf::Sprite& sprite = sprites.back();
+            sf::Texture& texture = textures.back();
+            AVSubtitleRect* subItem = sub->rects[i];
 
-				texture.create(subItem->w, subItem->h);
-				texture.setSmooth(true);
+            AVSubtitleType type = subItem->type;
 
-				uint32_t* data = new uint32_t[subItem->w* sub->rects[i]->h];
-				for (int j = 0; j < subItem->w * subItem->h; ++j)
-					data[j] = palette[subItem->pict.data[0][j]];
+            if (type == SUBTITLE_BITMAP)
+            {
+                positions.push_back(sf::Vector2i(subItem->x, subItem->y));
+                uint32_t* palette = new uint32_t[subItem->nb_colors];
+                for (int j = 0; j < subItem->nb_colors; j++)
+                    palette[j] = *(uint32_t*)&subItem->pict.data[1][j * RGBASize];
 
-				texture.update((uint8_t*)data);
-				sprite.setTexture(texture);
+                texture.create(subItem->w, subItem->h);
+                texture.setSmooth(true);
 
-				delete[] data;
-				delete[] palette;
-			}
-			else
-			{
-				//TODO: add libass code
-			}
-		}
-	}
+                uint32_t* data = new uint32_t[subItem->w* sub->rects[i]->h];
+                for (int j = 0; j < subItem->w * subItem->h; ++j)
+                    data[j] = palette[subItem->pict.data[0][j]];
 
-	void SubtitleStream::didStop(const Timer& timer, sfe::Status previousStatus)
-	{
-        while (m_visibleSubtitles.size()) {
+                texture.update((uint8_t*)data);
+                sprite.setTexture(texture);
+
+                delete[] data;
+                delete[] palette;
+            }
+            else
+            {
+                //TODO: add libass code
+            }
+        }
+    }
+
+    void SubtitleStream::didStop(const Timer& timer, sfe::Status previousStatus)
+    {
+        while (m_visibleSubtitles.size())
+        {
             delete m_visibleSubtitles.back();
             m_visibleSubtitles.pop_back();
         }
-        
-        while (m_pendingSubtitles.size()) {
+
+        while (m_pendingSubtitles.size())
+        {
             delete m_pendingSubtitles.back();
             m_pendingSubtitles.pop_back();
         }
-        
-		Stream::didStop(timer, previousStatus);
-	}
 
-	
+        Stream::didStop(timer, previousStatus);
+    }
 }
--- src/SubtitleStream.hpp
+++ src/SubtitleStream.hpp
@@ -36,64 +36,65 @@
 namespace sfe
 {
 
-	class SubtitleStream : public Stream{
-	public:
-		struct Delegate {
-			virtual void didUpdateSubtitle(const SubtitleStream& sender,
-                                           const std::list<sf::Sprite>& subimages,
-										   const std::list<sf::Vector2i>& positions) = 0;
-		};
-		/** Create a subtitle stream from the given FFmpeg stream
-		*
-		* At the end of the constructor, the stream is guaranteed
-		* to have all of its fields set and the decoder loaded
-		*/
-		SubtitleStream(AVFormatContext* formatCtx, AVStream* stream, DataSource& dataSource, Timer& timer, Delegate& delegate);
+    class SubtitleStream : public Stream{
+    public:
+        struct Delegate
+        {
+            virtual void didUpdateSubtitle(const SubtitleStream& sender,
+                const std::list<sf::Sprite>& subimages,
+                const std::list<sf::Vector2i>& positions) = 0;
+        };
+        /** Create a subtitle stream from the given FFmpeg stream
+        *
+        * At the end of the constructor, the stream is guaranteed
+        * to have all of its fields set and the decoder loaded
+        */
+        SubtitleStream(AVFormatContext* formatCtx, AVStream* stream, DataSource& dataSource, Timer& timer, Delegate& delegate);
 
-		/** Default destructor
-		*/
-		virtual ~SubtitleStream();
+        /** Default destructor
+        */
+        virtual ~SubtitleStream();
 
-		/** Get the stream kind (either audio, video or subtitle stream)
-		*
-		* @return the kind of stream represented by this stream
-		*/
-		virtual MediaType getStreamKind() const;
+        /** Get the stream kind (either audio, video or subtitle stream)
+        *
+        * @return the kind of stream represented by this stream
+        */
+        virtual MediaType getStreamKind() const;
 
-		/** Update the stream's status
-		*/
-		virtual void update();
-        
-		// Timer::Observer interface
-		void didStop(const Timer& timer, sfe::Status previousStatus);
+        /** Update the stream's status
+        */
+        virtual void update();
 
-	private:
-		/** The struct we use to store our subtitles
-		*/
-		struct SubtitleData
-		{
-			std::list<sf::Texture> textures;
+        // Timer::Observer interface
+        void didStop(const Timer& timer, sfe::Status previousStatus);
+
+    private:
+        /** The struct we use to store our subtitles
+        */
+        struct SubtitleData
+        {
+            std::list<sf::Texture> textures;
             std::list<sf::Sprite> sprites;
-			std::list<sf::Vector2i> positions;
-			//when will it appear (absolute)
-			sf::Time start;
-			//when will it disappear (absolute)
-			sf::Time end;
-			//Create our subtitle from an AVSubtitle
-			SubtitleData(AVSubtitle* sub);
-		};
-        
-		/** Decode the packages that were send to the stream by the demuxer
-		*
-		* @return if the stream is finished or not
-		*/
-		bool onGetData();
+            std::list<sf::Vector2i> positions;
+            //when will it appear (absolute)
+            sf::Time start;
+            //when will it disappear (absolute)
+            sf::Time end;
+            //Create our subtitle from an AVSubtitle
+            SubtitleData(AVSubtitle* sub);
+        };
 
-		Delegate& m_delegate;
-		
-		std::list<SubtitleData*> m_pendingSubtitles;
-		std::list<SubtitleData*> m_visibleSubtitles;
-	};
+        /** Decode the packages that were send to the stream by the demuxer
+        *
+        * @return if the stream is finished or not
+        */
+        bool onGetData();
+
+        Delegate& m_delegate;
+
+        std::list<SubtitleData*> m_pendingSubtitles;
+        std::list<SubtitleData*> m_visibleSubtitles;
+    };
 
 };
 
