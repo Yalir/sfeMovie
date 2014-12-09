78db5ec390b9e584c61039d6213b31ee730dabab
feliwir <stephan.vedder@gmail.com>
2014-12-09 10:32:32 +0100

    Changed the modulename of the atomic test

--- tests/Atomic.cpp
+++ tests/Atomic.cpp
@@ -1,11 +1,11 @@
 #define BOOST_TEST_MAIN
-#define BOOST_TEST_MODULE RangeBasedLoop
+#define BOOST_TEST_MODULE Atomic
 #include <boost/test/unit_test.hpp>
 #include <atomic>
 #include <iostream>
 //boost test didn't allow a multithreaded example...
 
-BOOST_AUTO_TEST_CASE(RangeBasedLoop)
+BOOST_AUTO_TEST_CASE(Atomic)
 {
 	std::atomic<int> num;
 	num = 0;
