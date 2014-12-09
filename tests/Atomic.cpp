c2c577f8b252c4b0bc29964088d7da44cd7826cd
feliwir <stephan.vedder@gmail.com>
2014-12-09 10:28:36 +0100

    Added a test for atomic types

---
+++ tests/Atomic.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE RangeBasedLoop
+#include <boost/test/unit_test.hpp>
+#include <atomic>
+#include <iostream>
+//boost test didn't allow a multithreaded example...
+
+BOOST_AUTO_TEST_CASE(RangeBasedLoop)
+{
+	std::atomic<int> num;
+	num = 0;
+	for(int i = 0;i<100;++i)
+	{
+	++num;
+	}
+	std::cout<< num << std::endl;
+	BOOST_CHECK(num == 100);
+}
