8f0e96b012b91c5f0eb293e02830547f8f816b61
Ceylo <lucas.soltic@orange.fr>
2014-12-21 14:11:04 +0100

    #65 Added move semantics test

--- tests/Atomic.cpp
+++ tests/Atomic.cpp
@@ -2,8 +2,6 @@
 #define BOOST_TEST_MODULE Atomic
 #include <boost/test/unit_test.hpp>
 #include <atomic>
-#include <iostream>
-//boost test didn't allow a multithreaded example...
 
 BOOST_AUTO_TEST_CASE(Atomic)
 {
