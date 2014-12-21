33987b9c9eab8803b9bda8c36ae45a0221777996
Ceylo <lucas.soltic@orange.fr>
2014-12-21 12:41:22 +0100

    #65 Small cleaning with std::atomic test

--- tests/Atomic.cpp
+++ tests/Atomic.cpp
@@ -7,12 +7,6 @@
 
 BOOST_AUTO_TEST_CASE(Atomic)
 {
-	std::atomic<int> num;
-	num = 0;
-	for(int i = 0;i<100;++i)
-	{
-	++num;
-	}
-	std::cout<< num << std::endl;
-	BOOST_CHECK(num == 100);
+    std::atomic<int> num(18);
+	BOOST_CHECK(num == 18);
 }
