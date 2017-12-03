5e2d7076813644f341d740baa49d95b59b1bc59d
Ceylo <lucas.soltic@orange.fr>
2017-12-03 21:20:57 +0100

    Another try for SFML_ROOT

--- ci/travis_setup.sh
+++ ci/travis_setup.sh
@@ -9,16 +9,18 @@ then
     brew install yasm wget
 
     # Download SFML
+    cd /tmp
     wget -q https://www.sfml-dev.org/files/SFML-2.4.2-osx-clang.tar.gz
     tar -xzf SFML-2.4.2-osx-clang.tar.gz
-    export SFML_ROOT=$(pwd)/SFML-2.4.2-osx-clang/Frameworks
+    ln -s $(pwd)/SFML-2.4.2-osx-clang/Frameworks /tmp/SFML_ROOT
 else
 	# Add dependencies
 	sudo apt-get update -qq
 	sudo apt-get install -y -qq yasm wget
 	
 	# Download SFML
+	cd /tmp
 	wget -q https://www.sfml-dev.org/files/SFML-2.4.2-linux-gcc-64-bit.tar.gz
 	tar -xzf SFML-2.4.2-linux-gcc-64-bit.tar.gz
-	export SFML_ROOT=$(pwd)/SFML-2.4.2
+	ln -s $(pwd)/SFML-2.4.2 /tmp/SFML_ROOT
 fi
