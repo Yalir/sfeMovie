f61ac8b4bec12a0d2ff054844bbe287350fc97dd
Ceylo <lucas.soltic@orange.fr>
2017-12-03 20:59:07 +0100

    Use a shell script

---
+++ ci/travis_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -ev
+
+os=$(uname)
+
+if [ "${os}" = "Darwin" ]
+then
+	# Add dependencies
+    brew install yasm wget
+
+    # Download SFML
+    wget https://www.sfml-dev.org/files/SFML-2.4.2-osx-clang.tar.gz
+    tar -xzf SFML-2.4.2-osx-clang.tar.gz
+    ln -s $(pwd)/SFML-2.4.2-osx-clang/Frameworks SFML_ROOT
+else
+	# Add dependencies
+	sudo apt-get update -qq
+	sudo apt-get install -yq yasm wget
+	
+	# Download SFML
+	wget https://www.sfml-dev.org/files/SFML-2.4.2-linux-gcc-64-bit.tar.gz
+	tar -xzf SFML-2.4.2-linux-gcc-64-bit.tar.gz
+	ln -s $(pwd)/SFML-2.4.2 SFML_ROOT
+fi
