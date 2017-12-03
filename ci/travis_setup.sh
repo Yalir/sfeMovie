b6ea3770a0915591dc552d84e29ceca9a6b29c51
Ceylo <lucas.soltic@orange.fr>
2017-12-03 22:58:39 +0100

    SFML website provides Linux binary with different compiler than on Travis, need to rebuild SFML

--- ci/travis_setup.sh
+++ ci/travis_setup.sh
@@ -16,11 +16,16 @@ then
 else
 	# Add dependencies
 	sudo apt-get update -qq
-	sudo apt-get install -y -qq yasm wget
+	sudo apt-get install -y -qq yasm wget unzip openal-dev freetype-dev jpeg-dev xrandr-dev xcb-dev xcb-randr-dev xcb-image-dev opengl-dev flac-dev ogg-dev vorbis-dev vorbisenc-dev vorbisfile-dev pthread-dev
 	
 	# Download SFML
 	cd /tmp
-	wget -q https://www.sfml-dev.org/files/SFML-2.4.2-linux-gcc-64-bit.tar.gz
-	tar -xzf SFML-2.4.2-linux-gcc-64-bit.tar.gz
-	ln -s $(pwd)/SFML-2.4.2 /tmp/SFML_ROOT
+	wget -q https://www.sfml-dev.org/files/SFML-2.4.2-sources.zip
+	unzip SFML-2.4.2-linux-gcc-64-bit.tar.gz
+	mkdir sfml-build && cd sfml-build
+	cmake /tmp/SFML-2.4.2 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+	make -j8
+	sudo make install
+
+	ln -s /usr/local /tmp/SFML_ROOT
 fi
