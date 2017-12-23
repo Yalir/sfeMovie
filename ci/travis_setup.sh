e05e3d2f568b3f549705532095c6149a954da12a
Ceylo <lucas.soltic@orange.fr>
2017-12-23 15:42:58 +0100

    Add missing sudo

--- ci/travis_setup.sh
+++ ci/travis_setup.sh
@@ -26,7 +26,7 @@ ctl.dummy {
 }
 EOF
     chmod go+r /home/travis/.asoundrc
-    cat << EOF >> /etc/modules.conf
+    sudo cat << EOF >> /etc/modules.conf
 # OSS/Free portion - card #1
 alias sound-slot-0 snd-card-0
 alias sound-service-0-0 snd-mixer-oss
@@ -35,7 +35,7 @@ alias sound-service-0-3 snd-pcm-oss
 alias sound-service-0-8 snd-seq-oss
 alias sound-service-0-12 snd-pcm-oss
 EOF
-    modprobe snd-dummy 
+    sudo modprobe snd-dummy 
     # ; modprobe snd-pcm-oss ; modprobe snd-mixer-oss ; modprobe snd-seq-oss
     mkdir -p tmp && chmod 777 tmp
 }
