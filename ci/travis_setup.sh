93f0c6be940f970d100518e82fa6e7783d958abf
Ceylo <lucas.soltic@orange.fr>
2017-12-24 15:11:13 +0100

    Get closer to original code for virtual audio device

--- ci/travis_setup.sh
+++ ci/travis_setup.sh
@@ -10,34 +10,10 @@ brew_if_needed()
 
 linux_sound_card()
 {
-    # From https://github.com/k3it/qsorder
+    # From https://github.com/k3it/qsorder/blob/master/.travis.yml
     sudo usermod -a -G audio travis
     sudo apt-get install -y -qq portaudio19-dev libasound2-dev alsa-utils alsa-oss
-
-    cat << EOF > /home/travis/.asoundrc
-pcm.dummy {
-  type hw
-  card 0
-}
-
-ctl.dummy {
-  type hw
-  card 0
-}
-EOF
-    chmod go+r /home/travis/.asoundrc
-    sudo cat << EOF >> /etc/modules.conf
-# OSS/Free portion - card #1
-alias sound-slot-0 snd-card-0
-alias sound-service-0-0 snd-mixer-oss
-alias sound-service-0-1 snd-seq-oss
-alias sound-service-0-3 snd-pcm-oss
-alias sound-service-0-8 snd-seq-oss
-alias sound-service-0-12 snd-pcm-oss
-EOF
-    sudo modprobe snd-dummy 
-    # ; modprobe snd-pcm-oss ; modprobe snd-mixer-oss ; modprobe snd-seq-oss
-    mkdir -p tmp && chmod 777 tmp
+    sudo bash prep-dummy-soundcard.sh
 }
 
 if [ "$(uname)" = "Darwin" ]
